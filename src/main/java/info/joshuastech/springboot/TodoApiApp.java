eb6c46710ccb47a26b4a76e6a9afb1aaffc68d19
Joshua Francis <joshua.mahendran@gmail.com>
2019-07-13 08:31:04 +1000

    Added Dockerfile, mvn plugin for docker

---
+++ src/main/java/info/joshuastech/springboot/TodoApiApp.java
@@ -0,0 +1,14 @@
+package info.joshuastech.springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TodoApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiApp.class, args);
+
+	}
+
+}
